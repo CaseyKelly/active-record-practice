4d15368a88dac8aca8cb60daee257570c83a7daa
CaseyKelly <casey.kelly@colorado.edu>
2015-02-05 15:15:20 -0700

    employees can be created for companies

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,6 +4,21 @@ class EmployeesController < ApplicationController
       @employees = Employee.all
     end
 
+    def new
+      @employee = Employee.new
+      @company = Company.find(params[:company_id])
+    end
+
+    def create
+      @employee = Employee.new(employee_params)
+      @employee.company_id = params[:company_id]
+      if @employee.save
+      redirect_to companies_path
+        else
+          render @employee
+      end
+    end
+
     def show
       @employee = Employee.find(params[:id])
     end
